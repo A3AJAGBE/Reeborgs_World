3663c184b1603f52b1ad2d856b8ffad30ce131a1
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-23 07:13:06 +0000

    Add hurdle 1 solution

--- main.py
+++ main.py
@@ -2,3 +2,24 @@
 This are some of the solution to Reeborg's world challenges. I used this to practice python functions
 """
 
+# Hurdle 1 Solution
+
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left() 
+    
+def jump():
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for i in range(6):
+    move()
+    turn_left()
+    move()
+    jump()
+"""

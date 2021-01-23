e3d4ef2e6cc6d9ef32ea514b999f57f23de6942c
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-23 08:34:59 +0000

    Add maze solution

--- main.py
+++ main.py
@@ -87,3 +87,22 @@ while not at_goal():
     else:
         move()
 """
+
+# Maze Solution (It's the same as hurdle 3 solution
+
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif wall_in_front():
+        turn_left()
+    else:
+        move()
+        
+"""
